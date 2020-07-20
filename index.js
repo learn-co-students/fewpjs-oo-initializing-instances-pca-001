a2c5592026995688ee4831be2df48bb53f416f4a
Idan <idan.zonshein@powerhrg.com>
2020-07-20 09:11:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food
+    this.drink = drink
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad
+    this.soup = soup
+    this.drink = drink
+  }
+}
+
+let firstLunch = new Lunch ("ceaser", "chicken", "water")
+
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad
+    this.soup = soup
+    this.entree = entree
+    this._dessert = dessert
+  }
+}
